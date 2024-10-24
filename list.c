a37c202aaadbf15ff6d0dde9a6560339e48903e2
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-10-24 15:16:22 -0400

    Add files via upload

---
+++ list.c
@@ -0,0 +1,298 @@
+// list/list.c
+// 
+// Implementation for linked list.
+//
+// Author: Danae Dunlap
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+list_t *list_alloc() { 
+  list_t* mylist =  (list_t *) malloc(sizeof(list_t)); 
+  mylist->head = NULL;
+  return mylist;
+}
+
+void list_free(list_t *l) {
+
+  if(list_length(l) == 0){
+    printf("List already empty\n"); 
+    return ; 
+  }
+
+  node_t* curr = l->head; 
+  node_t* temp; 
+
+  
+  while(curr != NULL){ 
+    temp = curr->next; 
+    free(curr); 
+    curr = temp; 
+  }
+
+  l->head = NULL; 
+}
+
+
+void list_print(list_t *l){
+  node_t* curr = l->head;
+  printf("My List: \n"); 
+  while (curr != NULL){
+    printf("%d\n", curr->value);
+    curr = curr->next;
+  }
+}
+
+char * listToString(list_t *l) {
+  char* buf = (char *) malloc(sizeof(char) * 1024);
+  char tbuf[20];
+
+	node_t* curr = l->head;
+  while (curr != NULL) {
+    sprintf(tbuf, "%d->", curr->value);
+    curr = curr->next;
+    strcat(buf, tbuf);
+  }
+  strcat(buf, "NULL");
+  return buf;
+}
+
+int list_length(list_t *l) { 
+  int listlen = 0; 
+  node_t* curr = l->head;
+  while(curr != NULL){
+    listlen++; 
+    curr = curr->next;
+  }
+
+  return listlen; 
+}
+
+void list_add_to_back(list_t *l, elem value) {
+  node_t* newNode = (node_t *) malloc(sizeof(node_t)); 
+  node_t* curr = l->head;
+
+  newNode->value = value;
+  newNode->next = NULL;
+
+  if(curr != NULL){
+    while(curr->next != NULL){
+    curr = curr->next;
+    }
+    curr->next = newNode; 
+  }else{
+    l->head = newNode; 
+  }
+
+}
+
+void list_add_to_front(list_t *l, elem value) {
+    node_t* cur_node = (node_t *) malloc(sizeof(node_t));
+    node_t* head = l->head;  // get head of list
+    
+    cur_node->value = value;
+
+     /* Insert to front */
+     cur_node->next = head;
+     l->head = cur_node;
+}
+
+void list_add_at_index(list_t *l, elem value, int index){
+
+  if(l == NULL){
+    printf("List is not intialized"); 
+    return; 
+  }
+
+  node_t* curr = l->head;
+  node_t* prev = NULL;
+
+  node_t* newNode = (node_t *) malloc(sizeof(node_t));
+  if(newNode == NULL){
+    printf("Memory allocation failed\n"); 
+    return; 
+  }
+
+  newNode->value = value; 
+  newNode->next = NULL; 
+
+  int listlen = list_length(l); 
+  if(index < 0){
+    index = listlen + index + 1; 
+    if(index < 0){
+      printf("Given index surpasses current list length\n"); 
+      return; 
+    }
+  }
+
+  if(index > listlen){
+    printf("Given index surpasses current list length\n"); 
+    return; 
+  }else if(index == 0){
+    newNode->next = l->head; 
+    l->head=newNode; 
+  }else if(index == listlen){
+    list_add_to_back(l, value); 
+  }
+  else{
+    int i; 
+    for(i = 0; i < index; i++){
+      prev = curr; 
+      curr = curr->next; 
+    }
+    prev->next = newNode; 
+    newNode->next = curr;
+  }
+}
+
+elem list_remove_from_back(list_t *l) { 
+
+  if(l->head == NULL){
+    printf("List is empty\n"); 
+    return -1; 
+  }
+
+  node_t* curr = l->head; 
+  node_t* prev = NULL;
+
+  int listlen = list_length(l); 
+  elem value; 
+
+  if(listlen == 1){
+    value = l->head->value; 
+    l->head = NULL;
+    return value; 
+  }
+  
+  while(curr->next->next != NULL){
+    prev = curr; 
+    curr = curr->next; 
+  }
+
+  value = curr->value;
+  prev->next = NULL;
+  return value; 
+
+}
+
+elem list_remove_from_front(list_t *l) { 
+  if(l->head == NULL || l == NULL){
+    return -1; 
+  }
+
+  node_t *first = l->head; 
+  elem value = first->value; 
+
+  l->head = first->next;
+
+  return value;
+}
+
+elem list_remove_at_index(list_t *l, int index) { 
+  node_t* curr = l->head; 
+  node_t* prev = l->head;
+  node_t* removed; 
+  int listlen = list_length(l);
+
+  if(listlen == 0){
+    printf("List already empty\n"); 
+    return -1; 
+  }
+
+  if(index < 0){
+    index = listlen + index + 1; 
+    if(index < 0){
+      printf("Given index is greater than current length of list"); 
+      return -1; 
+    }
+  }
+
+  if(index >= listlen){
+    printf("Given index is greater than current length of list"); 
+    return -1; 
+  }
+
+  if(index == 0){
+    list_remove_from_front(l); 
+  }
+
+  if(index == listlen-1){
+    list_remove_from_back(l); 
+  }
+
+  int i; 
+  for(i = 0; i < index; i++){
+    prev = curr; 
+    curr = curr->next; 
+  }
+
+  if(curr){
+    prev->next = curr->next; 
+    removed = curr; 
+    return removed->value; 
+  }
+
+  return -1; 
+}
+
+
+bool list_is_in(list_t *l, elem value) { 
+  node_t* curr = l->head;
+
+  while(curr != NULL){
+    if (curr->value == value){
+      return true; 
+    }
+    curr = curr->next;
+  } 
+  return false; 
+}
+
+elem list_get_elem_at(list_t *l, int index) { 
+  node_t* curr = l->head;
+  int listlen = list_length(l); 
+
+  if(index >= listlen){
+    printf("Given index is greater than current list length\n"); 
+    return -1; 
+  }
+
+  if(index < 0){
+    index = listlen + index; 
+  
+    if(index < 0){
+      printf("Given index is greater than current list length\n"); 
+      return -1; 
+    }
+  }
+
+  int i; 
+  for(i = 0; i < index; i++){
+    if(curr == NULL){
+      break; 
+    }
+    curr = curr->next; 
+  }
+
+  return curr->value; 
+}
+
+
+int list_get_index_of(list_t *l, elem value) { 
+  node_t* curr = l->head;
+  int index = 0; 
+
+  while(curr != NULL){
+    if(curr->value == value){
+      return index; 
+    }
+    curr = curr->next;
+    index++; 
+  } 
+
+  printf("Value is not in linked list\n"); 
+  return -1; 
+}
+
---
+++ serv.c
@@ -0,0 +1,122 @@
+#include <netinet/in.h> //structure for storing address information 
+#include <stdio.h> 
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h> 
+#include <sys/socket.h> //for socket APIs 
+#include <sys/types.h> 
+#include <unistd.h>
+#include "list.h"
+
+#define PORT 9001
+#define ACK "ACK"
+
+// list
+list_t *mylist;
+
+void signal_handler(int signal); 
+  
+int main(int argc, char const* argv[]) 
+{ 
+  
+	  int n, val, idx;
+    // create server socket similar to what was done in 
+    // client program 
+    int servSockD = socket(AF_INET, SOCK_STREAM, 0); 
+  
+    // string store data to recv/send to/from client 
+    char buf[1024];
+		char sbuf[1024];
+    char* token;
+
+    // define server address 
+    struct sockaddr_in servAddr; 
+	
+    servAddr.sin_family = AF_INET; 
+    servAddr.sin_port = htons(PORT); 
+    servAddr.sin_addr.s_addr = INADDR_ANY; 
+  
+    // bind socket to the specified IP and port 
+    bind(servSockD, (struct sockaddr*)&servAddr, 
+         sizeof(servAddr)); 
+  
+    // listen for connections 
+    listen(servSockD, 1); 
+  
+    // integer to hold client socket. 
+    int clientSocket = accept(servSockD, NULL, NULL); 
+
+    mylist = list_alloc();  // create the list
+		signal(SIGINT, signal_handler); 
+
+    while(1){
+        // recvs messages from client socket 
+        n = recv(clientSocket, buf, sizeof(buf), 0);
+				buf[n] = '\0';
+
+        if(n > 0){
+            token = strtok(buf, " ");
+    
+						if(strcmp(token,"exit") == 0){
+							  list_free(mylist);
+                close(clientSocket); 
+							  exit(0);
+						}
+						else if(strcmp(token, "get") == 0){
+							token = strtok(NULL, " "); 
+							idx = atoi(token); 
+							val = list_get_elem_at(mylist, idx); 
+							sprintf(sbuf, "%s%d", "Value: ", val); 
+						}
+						else if(strcmp(token,"get_length") == 0){
+								// Make a Call
+								val = list_length(mylist);
+								sprintf(sbuf,"%s%d", "Length = ", val);
+						}
+						else if(strcmp(token,"add_front") == 0){
+							  token = strtok(NULL, " ");  // get next token (value)
+								val = atoi(token);
+								list_add_to_front(mylist,val);
+                sprintf(sbuf, "ACK %s", ACK);
+						}else if(strcmp(token,"add_back") == 0){
+							token = strtok(NULL, " "); 
+							val = atoi(token); 
+							list_add_to_back(mylist, val);
+							sprintf(sbuf, "ACK %s", ACK);
+						}
+						else if(strcmp(token,"remove_position") == 0){
+							  token = strtok(NULL, " ");
+								idx = atoi(token);
+								val = list_remove_at_index(mylist,idx);
+								sprintf(sbuf,"Removed: %d", val);
+						}else if(strcmp(token, "add_position") == 0){
+							token = strtok(NULL, " ");
+							idx = atoi(token);  
+							token = strtok(NULL, " ");
+							val = atoi(token); 
+							list_add_at_index(mylist, idx, val);
+							
+						}
+						else if(strcmp(token,"print") == 0){
+							  sprintf(sbuf,"%s", listToString(mylist));
+						}else if(strcmp(token, "remove_back") == 0){
+							val = list_remove_from_back(mylist);
+							sprintf(sbuf,"Removed: %d", val);
+						}else if(strcmp(token, "remove_front")== 0){
+							token = strtok(NULL, " ");
+							val = list_remove_from_front(mylist);
+							sprintf(sbuf,"Removed: %d", val);
+						}
+                // send's messages to client socket 
+            send(clientSocket, sbuf, sizeof(sbuf), 0);
+        }
+				memset(buf, '\0', 1024);
+    }
+    return 0; 
+}
+
+void signal_handler(int signal){
+	printf("Caught signal %d, terminating server...\n", signal); 
+	list_free(mylist); 
+	exit(0); 
+}
